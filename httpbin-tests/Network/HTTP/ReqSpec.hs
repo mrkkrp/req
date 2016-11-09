3a7fc0c098b5caa9994b05b84631ec16e3b9fc6f
mrkkrp <markkarpov@opmbx.org>
2016-11-09 13:48:09 +0300

    Start writing the ‘httpbin-tests’ test suite

--- httpbin-tests/Network/HTTP/ReqSpec.hs
+++ httpbin-tests/Network/HTTP/ReqSpec.hs
@@ -31,11 +31,96 @@
 -- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
+{-# LANGUAGE CPP                  #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Network.HTTP.ReqSpec
   ( spec )
 where
 
+import Control.Exception (throwIO)
+import Control.Monad.Reader
+import Data.Aeson (Value (..), object, (.=))
+import Data.HashMap.Strict (HashMap)
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import Network.HTTP.Req
 import Test.Hspec
+import qualified Data.HashMap.Strict as HM
+
+#if !MIN_VERSION_base(4,8,0)
+import Data.Monoid (mempty)
+#endif
 
 spec :: Spec
-spec = return ()
+spec = do
+
+  describe "exception throwing on non 2xx-status codes" $ do
+    it "doesn't throw for 200" pending
+    it "throws indeed for non-404" pending
+
+  describe "response check via httpConfigCheckResponse" $
+    context "if it's set to always throw" $
+      it "throws indeed" pending
+
+  describe "receiving user-agent header back" $
+    it "works" $ do
+      r <- req GET (httpbin /: "user-agent")
+        NoReqBody jsonResponse (header "user-agent" "Req")
+      responseBody          r `shouldBe` object
+        ["user-agent" .= ("Req" :: Text)]
+      responseStatusCode    r `shouldBe` 200
+      responseStatusMessage r `shouldBe` "OK"
+
+  describe "receiving request headers back" $
+    it "works" $ do
+      r <- req GET (httpbin /: "headers")
+        NoReqBody jsonResponse (header "Foo" "bar" <> header "Baz" "quux")
+      responseBody          r `shouldBe` object
+        [ "headers" .= object
+          [ "Accept-Encoding" .= ("gzip"        :: Text)
+          , "Foo"             .= ("bar"         :: Text)
+          , "Baz"             .= ("quux"        :: Text)
+          , "Host"            .= ("httpbin.org" :: Text) ] ]
+      responseStatusCode    r `shouldBe` 200
+      responseStatusMessage r `shouldBe` "OK"
+
+  describe "receiving GET data back" $
+    it "works" $ do
+      r <- req GET (httpbin /: "get") NoReqBody jsonResponse mempty
+      stripOrigin (responseBody r) `shouldBe` object
+        [ "args" .= (HM.empty :: HashMap Text Text)
+        , "url"  .= ("https://httpbin.org/get" :: Text)
+        , "headers" .= object
+          [ "Accept-Encoding" .= ("gzip"        :: Text)
+          , "Host"            .= ("httpbin.org" :: Text) ] ]
+      responseStatusCode      r `shouldBe` 200
+      responseStatusMessage   r `shouldBe` "OK"
+
+----------------------------------------------------------------------------
+-- Instances
+
+instance MonadHttp IO where
+  handleHttpException = throwIO
+
+instance MonadHttp (ReaderT HttpConfig IO) where
+  handleHttpException = liftIO . throwIO
+  getHttpConfig       = ask
+
+----------------------------------------------------------------------------
+-- Helpers
+
+-- | 'Url' representing <https://httpbin.org>.
+
+httpbin :: Url 'Https
+httpbin = https "httpbin.org"
+
+-- | Remove “origin” field from JSON value. Origin may change, we don't want
+-- to depend on that.
+
+stripOrigin :: Value -> Value
+stripOrigin (Object m) = Object (HM.delete "origin" m)
+stripOrigin value      = value
